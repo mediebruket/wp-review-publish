0f402c219fdba9c8657539d5bd786e20eba85ae3
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-01-04 02:21:51 +0100

    ensure metadata is saved if post is saved as draft

--- wp-review-publish.php
+++ wp-review-publish.php
@@ -124,10 +124,6 @@ function process_book_review_fields( $book_review_id, $book_review ) {
 	if ( $book_review->post_type != 'book_reviews' )
 		return;
 
-	// Don't do anything if post is draft
-	if ( $book_review->post_status != "publish" )
-		return;
-
 	// Store data in meta table if present in post data
 	if ( isset( $_POST['book_review_book_isbn'] ) && $_POST['book_review_book_isbn'] != '' ) {
 		update_post_meta( $book_review_id, 'book_isbn', $_POST['book_review_book_isbn'] );
@@ -144,6 +140,10 @@ function process_book_review_fields( $book_review_id, $book_review ) {
 	if ( isset( $_POST['review_audience'] ) && $_POST['review_audience'] != '0') {
 		update_post_meta( $book_review_id, 'review_audience', $_POST['review_audience'] );
 	}
+
+	// Don't do continue if post is draft
+	if ( $book_review->post_status != "publish" )
+		return;
 
 	// set up HTTP request for push data.deichman.no
 	if( !class_exists( 'WP_Http' ) ) {
